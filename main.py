bd3ace8576ae1c8c31f1afc6589caf5d917e6089
haley-mcquick <72761290+haley-mcquick@users.noreply.github.com>
2021-01-01 23:28:24 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,7 @@
+while True:
+    print(input.light_level)
+
+if input.light_level() < 6:
+    light.set_all(color.rgb(255, 0, 0))
+else:
+    light.set_all(color.rgb(0, 255, 0))
---
+++ main.ts
@@ -0,0 +1,9 @@
+while (true) {
+    console.log(input.lightLevel)
+}
+if (input.lightLevel() < 6) {
+    light.setAll(color.rgb(255, 0, 0))
+} else {
+    light.setAll(color.rgb(0, 255, 0))
+}
+
