43022b20ac1278b96ef94cfe9f9172b1b6550bdb
haley-mcquick <72761290+haley-mcquick@users.noreply.github.com>
2021-01-02 00:37:03 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 while True:
     print(input.light_level())
-
-if input.light_level() < 6:
-    light.set_all(color.rgb(255, 0, 0))
-else:
-    light.set_all(color.rgb(0, 255, 0))
+    light.set_brightness(3)
+    if input.light_level() < 6:
+        light.set_all(color.rgb(255, 0, 0))
+    else:
+        light.set_all(color.rgb(0, 255, 0))
--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 while (true) {
     console.log(input.lightLevel())
+    light.setBrightness(3)
+    if (input.lightLevel() < 6) {
+        light.setAll(color.rgb(255, 0, 0))
+    } else {
+        light.setAll(color.rgb(0, 255, 0))
+    }
+    
 }
-if (input.lightLevel() < 6) {
-    light.setAll(color.rgb(255, 0, 0))
-} else {
-    light.setAll(color.rgb(0, 255, 0))
-}
-
