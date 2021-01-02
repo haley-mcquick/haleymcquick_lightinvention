6031267b403f28f933c3523f9a6517df5f9d07e4
haley-mcquick <72761290+haley-mcquick@users.noreply.github.com>
2021-01-02 14:41:52 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ while True:
     light.set_brightness(3)
     if input.light_level() < 6:
         light.set_all(color.rgb(255, 0, 0))
+    elif input.light_level() > 15:
+        light.set_all(color.rgb(255, 255, 0))
     else:
         light.set_all(color.rgb(0, 255, 0))
--- main.ts
+++ main.ts
@@ -3,6 +3,8 @@ while (true) {
     light.setBrightness(3)
     if (input.lightLevel() < 6) {
         light.setAll(color.rgb(255, 0, 0))
+    } else if (input.lightLevel() > 15) {
+        light.setAll(color.rgb(255, 255, 0))
     } else {
         light.setAll(color.rgb(0, 255, 0))
     }
